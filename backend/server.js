a3bfbaf6202037fc4620fce78bece5c84ca8b435
mahmoud-149 <mahmoud14ahmedd@gmail.com>
2026-08-19 18:54:07 +0300

    task 6 complete

--- backend/server.js
+++ backend/server.js
@@ -39,5 +39,8 @@ app.get('/test-db', (req, res) => {
   }
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`))
